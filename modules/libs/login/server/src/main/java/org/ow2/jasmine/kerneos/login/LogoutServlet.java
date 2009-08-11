e44c4c7bd704f1db8ccb21431260abbfa17979ae
nicoulaj <nicoulaj@0ed64165-3322-0410-a05a-e88151b9e9ba>
2009-08-11 09:55:58 +0000

    "Logout" button is back, but it does not work.
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/jasmine/kerneos/trunk@4867 0ed64165-3322-0410-a05a-e88151b9e9ba

---
+++ modules/libs/login/server/src/main/java/org/ow2/jasmine/kerneos/login/LogoutServlet.java
@@ -0,0 +1,58 @@
+/**
+ * JASMINe
+ * Copyright (C) 2009 Bull S.A.S.
+ * Contact: jasmine@ow2.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * --------------------------------------------------------------------------
+ * $Id$
+ * --------------------------------------------------------------------------
+ */
+package org.ow2.jasmine.kerneos.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * A servlet that ends the user session
+ *
+ * @author Julien Nicoulaud
+ */
+public class LogoutServlet extends HttpServlet {
+
+    /**
+     * Class version ID
+     */
+    private static final long serialVersionUID = -4729955174780787192L;
+
+    /**
+     * Handle GET requests
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+}
