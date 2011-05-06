8ac1513083929922311bc7bdeafef9f28eca21e4
diorcety <diorcety@0ed64165-3322-0410-a05a-e88151b9e9ba>
2011-05-06 09:28:28 +0000

    Set target of annotations
    git-svn-id: svn+ssh://svn.forge.objectweb.org/svnroot/jasmine/kerneos/trunk@8111 0ed64165-3322-0410-a05a-e88151b9e9ba

--- modules/core/server/src/main/java/org/ow2/jasmine/kerneos/service/KerneosService.java
+++ modules/core/server/src/main/java/org/ow2/jasmine/kerneos/service/KerneosService.java
@@ -25,10 +25,13 @@
 
 package org.ow2.jasmine.kerneos.service;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface KerneosService {
     String destination();
 
